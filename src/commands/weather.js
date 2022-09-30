c826dd6a599632208f29fcd5a35f9647d1484df8
notABluesSinger <rob@whatagoodbot.com>
2022-09-30 23:46:07 +0100

    Add defensiveness around weather image string

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -3,6 +3,11 @@ import { stringsDb } from '../models/index.js'
 
 export default async (options) => {
   if (!options.argument) return { message: await stringsDb.get('noCity') }
-  const url = buildUrl('wttr.in', [`${options.argument}_q0np.png`]).href
+  const args = options.argument.map(arg => {
+    return arg.replaceAll(/,/gi, '').replaceAll(/ /gi, '')
+  }).filter(arg => {
+    return arg.length > 0
+  })
+  const url = buildUrl('wttr.in', [`${args.join(',')}_q0np.png`]).href
   return { images: [url] }
 }
