7839535181b9c59a23cd67d9fe4d70413754a511
Rob Johnson <rob@fourjohnsons.com>
2023-07-13 18:28:56 +0100

    only greet users with nicknames

--- src/handlers/userJoined.js
+++ src/handlers/userJoined.js
@@ -5,13 +5,15 @@ export default async (payload, room) => {
   if (!payload.userUuid) return
   if ([process.env.CHAT_USER_ID, process.env.CHAT_REPLY_ID].includes(payload.userUuid)) return
   const userProfile = await getTTUser(payload.userUuid)
-  postMessage({
-    room,
-    message: `Welcome @${userProfile.nickname}... feel free to ask me any questions!`,
-    mentions: [{
-      position: 8,
-      nickname: userProfile.nickname,
-      userId: payload.userUuid
-    }]
-  })
+  if (userProfile.nickname.length > 0) {
+    postMessage({
+      room,
+      message: `Welcome @${userProfile.nickname}... feel free to ask me any questions!`,
+      mentions: [{
+        position: 8,
+        nickname: userProfile.nickname,
+        userId: payload.userUuid
+      }]
+    })
+  }
 }
